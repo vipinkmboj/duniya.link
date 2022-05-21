076d60992fffdacc7a3a3c48c8d0b58170bef637
Vipin Kumar <vipinkmboj21@gmail.com>
2022-05-21 13:25:32 +0530

    big change

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 //app.use('/posts', postRoutes); must be after or below(not above) app.use(cors()); otherwise it will create error in client side.
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = process.env.CONNECTIONURL;//'mongodb+srv://DuniyaLinkDataBaseUser:8R7wuCHpvrFXeJuS@clusterduniyalink.tq2ah.mongodb.net/duniyalinkdatabase?retryWrites=true&w=majority'//process.env.CONNECTIONURL; //'mongodb://localhost:27017/mernstackdatabase';
+const CONNECTION_URL = process.env.CONNECTIONURL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
